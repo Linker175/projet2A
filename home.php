d0105b4275ad16262017afffc2e08d40191b6583
Yoann <thebaultyoann@gmail.com>
2023-01-01 20:26:01 +0100

    Ajout page vitrine avec accès connexion

---
+++ home.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style_home.css" />
+        <title>Park'ENSEA</title>
+        <link rel="icon" type="image/png" sizes="64x64" href="image_onglet.png">
+    </head>
+    <body style="background: rgb(255 ,255 ,255);">
+
+        <h1>
+            Park'ENSEA
+        </h1>
+
+        <!-- acces a l'interface de connexion -->
+        <div class="Connexion">
+            <a class="connexion" href="login.php" target="_blank">
+                Se connecter
+            </a>
+        </div>
+                    
+    </body>
+
+    
