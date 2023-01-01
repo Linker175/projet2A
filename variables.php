ba417fb97d2c87901088f38871b83b99b8536a72
Yoann <thebaultyoann@gmail.com>
2023-01-01 20:37:46 +0100

    Ajout d'un variable pour stocker les données utilisateurs en clair

---
+++ variables.php
@@ -0,0 +1,17 @@
+<?php
+
+$users = [
+    [
+        'full_name' => 'Perez Lucie',
+        'email' => 'lucie.perez@exemple.com',
+        'password' => 'nice56',
+        'user_type' => 'user'
+    ],
+    [
+        'full_name' => 'Yoann Thébault',
+        'email' => 'thebault.yoann@exemple.com',
+        'password' => 'nice72',
+        'user_type' => 'admin'
+    ],
+];
+?>
