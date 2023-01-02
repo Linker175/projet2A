0d03c825ae8f91029f65dfe801b39653dabfc458
Yoann <thebaultyoann@gmail.com>
2023-01-02 23:49:57 +0100

    mise à jour pour css

--- delete_arceau_validation.php
+++ delete_arceau_validation.php
@@ -26,41 +26,56 @@ if (isset($arceau['lattitude']) || isset($arceau['longitude']) || isset($arceau[
 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="style_admin_page.css"  type="text/css"/>
+    <link rel="stylesheet" href="update_arceau.css"  type="text/css"/>
     <title>Validation de la suppression </title>
 </head>
 
 
 <!-------------- BODY -------------->
 
-<body class="update_body">
-    <div class="container">
-        <h1>Voulez vous vraiment supprimer cet arceau ?</h1>
+<body>
 
+    <div class=top>
+            <h1>
+                Suppression d'un Arceau
+            </h1>
+    </div>
+
+    <div class="interface">
+        <div class="ajout">
+        <h1>Voulez vous vraiment supprimer cet arceau ?</h1>
+    <h3>
      <?php echo ('Latitude :'.$arceau['lattitude'])."<br>"; ?>
      <?php echo ('Longitude :'.$arceau['longitude'])."<br>"; ?>
      <?php echo ('État :'.$arceau['etat'])."<br>"; ?>
      <?php echo ('Utilisateur :'.$arceau['id_user'])."<br>"; ?>
      <?php echo ('Groupe :'.$arceau['groupe'])."<br>"; ?>
-
-     <a href="//localhost/projet/delete_arceau_validation2.php?id=<?php echo($arceau['id_arceau']);?>" class="bouton_rouge"> SUPPRIMER </a>
-
+    </h3>
+        </div>
+    <div class=boutondel>
+        <a class="boutondelete" href="//localhost/projet/delete_arceau_validation2.php?id=<?php echo($arceau['id_arceau']);?>">Supprimer</a>
     </div>
-    <br>
-    <a href="//localhost/projet/admin_page.php"> Retour à la page d'administration </a>
-    </br>
+    </div>
+  
+    <div class="return">
+            <a class="bouton" href="//localhost/projet/ajout_arceau.php"> Retour à la page d'administration </a>
+    </div>
+  
 </body>
 
 <?php
 }
 else{
     echo('il faut des données fournies qui respectent le format demandé');
-
 ?>
-    <div class=boutton>
+<div class=return>
+    <div class=bouton>
         <a href="//localhost/projet/ajout_arceau.php"> Retour à la page d'administration </a>
     </div>
+</div>
 
 <?php
 }
 ?>
+
+</html>
