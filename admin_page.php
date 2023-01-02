5d6b6a76d58c2a81cc47c1a36bbc46b13d1626eb
Yoann <thebaultyoann@gmail.com>
2023-01-02 15:25:25 +0100

    Mise à jour de la page pour contenir une rédirection vers l'ajout d'un arceau plutôt que l'ajout de l'arceau directement sur la page

--- admin_page.php
+++ admin_page.php
@@ -29,47 +29,14 @@ include_once('./config_mysql.php');
     ?>
     </div>
 
-<!-------------- AJOUTS A LA BASE DE DONNE --------------> 
 
-<form action="ajout_arceau.php" method="post">
+<!-------------- AJOUT D'UN ARCEAU --------------> 
 
-    <?php if(isset($errorMessage)) : ?>
-    
-        <div class="alert alert-danger" role="alert">
-            <?php echo $errorMessage; ?>
-        </div>
-
-    <?php endif; ?>
-
-    <div class="coords">
-        <label for="latitude" class="form-label">Latitude</label>
-        <input type="number" class="form-control" id="latitude_add" name="latitude" placeholder="+/- 00.0000000">
-
-        <label for="longitude" class="form-label">Longitude</label>
-        <input type="number" class="form-control" id="longitude_add" name="longitude"  placeholder="+/- 00.0000000">
+    <div class=boutton>
+        <a href="//localhost/projet/ajout_arceau.php"> Ajouter un arceau </a>
     </div>
-
-    <div class="etat">
-        <label for="etat" class="form-label">État</label>
-        <input type="number" class="form-control" id="etat_add" name="etat"  placeholder="0 or 1">
-    </div>
-
-    <div class="utilisateur">
-        <label for="utilisateur" class="form-label">Utilisateur</label>
-        <input type="number" class="form-control" id="utilisateur_add" name="utilisateur"  placeholder="1">
-    </div>
-
-    <div class="groupe">
-        <label for="groupe" class="form-label">Groupe</label>
-        <input type="number" class="form-control" id="groupe_add" name="groupe"  placeholder="1">
-    </div>
-
-    <button type="submit" class="btn btn-primary">Envoyer</button>
-</form>
-
 
 <!-------------- AFFICHAGE DE LA BASE DE DONNEE --------------> 
-
 
 <!-- On prépare la requête -->
 
@@ -85,10 +52,10 @@ include_once('./config_mysql.php');
         <thead>
             <tr>
                 <th scope="col"> Coordonnées </th>
+                <th scope="col"> État</th>
+                <th scope="col"> Utilisateur</th>
+                <th scope="col"> Groupes </th>
                 <th scope="col"> Id </th>
-                <th scope="col"> État </th>
-                <th scope="col"> Utilisateur </th>
-                <th scope="col"> Groupe </th>
             </tr>
         </thead>
   
@@ -97,10 +64,11 @@ include_once('./config_mysql.php');
         <tr>    
             <? while($row = $req->fetch()) { ?>
             <td><? echo "{ ".$row['longitude']." ; ".$row['lattitude']." }"; ?></td>
-            <td><? echo $row['id_arceau']; ?></td>
             <td><? echo $row['etat']; ?></td>
             <td><? echo $row['id_user']; ?></td>
             <td><? echo $row['groupe']; ?></td>
+            <td><? echo $row['id_arceau']; ?></td>
+            <td><?  ?></td>
         </tr>
     
 
