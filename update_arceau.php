41affc43327ad88bb60fd16918dd0adcf26aa097
Yoann <thebaultyoann@gmail.com>
2023-01-02 23:50:25 +0100

    mise à jour pour css

--- update_arceau.php
+++ update_arceau.php
@@ -27,12 +27,21 @@ $arceau = $retrieveArceau->fetch(PDO::FETCH_ASSOC);
 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="style_admin_page.css"  type="text/css"/>
+    <link rel="stylesheet" href="update_arceau.css"  type="text/css"/>
     <title>Modification Arceau</title>
 </head>
 
+<!-------------- BODY -------------->
+<body>
+
+    <div class=top>
+            <h1>
+                Modification d'un Arceau
+            </h1>
+    </div>
 
 <!-------------- FORMULAIRE DE MODIFCATION DE LA BASE DE DONNE --------------> 
+<div class="interface">
 
 <form action="update_arceau_validation.php?id=<?php echo($getData['id'])?>" method="post">
     <?php if(isset($errorMessage)) : ?>
@@ -43,35 +52,46 @@ $arceau = $retrieveArceau->fetch(PDO::FETCH_ASSOC);
 
     <?php endif; ?>
 
-    <div class="coords">
-        <label for="lattitude" class="form-label">Latitude</label>
-        <input type="number" class="form-control" id="latitude_add" name="lattitude" step="0.0000001" placeholder="+/- 00.0000000", value= <?php echo($arceau['lattitude']); ?>>
+    <div class="lattitude">
+        <label for="lattitude" class="form_text">Latitude</label>
+        <input type="number" class="form" id="latitude_add" name="lattitude" step="0.0000001" placeholder="+/- 00.0000000", value= <?php echo($arceau['lattitude']); ?>>
+    </div>
 
-        <label for="longitude" class="form-label">Longitude</label>
-        <input type="number" class="form-control" id="longitude_add" name="longitude" step="0.0000001" placeholder="+/- 00.0000000", value= <?php echo($arceau['longitude']); ?>>
+    <div class="longitude">
+        <label for="longitude" class="form_text">Longitude</label>
+        <input type="number" class="form" id="longitude_add" name="longitude" step="0.0000001" placeholder="+/- 00.0000000", value= <?php echo($arceau['longitude']); ?>>
     </div>
 
     <div class="etat">
-        <label for="etat" class="form-label">État</label>
-        <input type="number" class="form-control" id="etat_add" name="etat"  placeholder="0 or 1", value= <?php echo($arceau['etat']); ?>>
+        <label for="etat" class="form_text">État</label>
+        <input type="number" class="form" id="etat_add" name="etat"  placeholder="0 or 1", value= <?php echo($arceau['etat']); ?>>
     </div>
 
-    <div class="id_user">
-        <label for="id_user" class="form-label">Utilisateur</label>
-        <input type="number" class="form-control" id="utilisateur_add" name="id_user"  placeholder="1", value= <?php echo($arceau['id_user']); ?>>
+    <div class="utilisateur">
+        <label for="id_user" class="form_text">Utilisateur</label>
+        <input type="number" class="form" id="utilisateur_add" name="id_user"  placeholder="1", value= <?php echo($arceau['id_user']); ?>>
     </div>
 
     <div class="groupe">
-        <label for="groupe" class="form-label">Groupe</label>
-        <input type="number" class="form-control" id="groupe_add" name="groupe"  placeholder="1", value= <?php echo($arceau['groupe']); ?>>
-    </div>
+        <label for="groupe" class="form_text">Groupe</label>
+        <input type="number" class="form" id="groupe_add" name="groupe"  placeholder="1", value= <?php echo($arceau['groupe']); ?>>
+    </div> 
 
-    <button type="submit" class="btn btn-primary">Modifier</button>
-    <a class="button_delete" href="./delete_arceau_validation.php?id=<?php echo($getData['id']); ?>">Supprimer</a>
+    <div class="boutons">
+        <div class="send">
+                <button type="submit" class="bouton">Modifier</button>
+        </div>
+        <div class="send2">
+            <a class="boutondelete" href="./delete_arceau_validation.php?id=<?php echo($getData['id']); ?>">Supprimer</a>
+        </div>
+    </div>
 </form>
 
+</div>
 
+<div class="return">
+        <a class="bouton" href="//localhost/projet/admin_page.php"> Retour à la page d'administration </a>
+</div>
 
-<br>
-<a href="//localhost/projet/admin_page.php"> Retour à la page d'administration </a>
-
+</body>
+</html>
