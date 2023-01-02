2482c871d6776ad941847577e446052f438ea48b
Yoann <thebaultyoann@gmail.com>
2023-01-02 16:38:12 +0100

    ajout d'une fonctionnalité de modification des données de l'arceau

---
+++ update_arceau.php
@@ -0,0 +1,74 @@
+<?php
+session_start();
+include_once('./config_mysql.php');
+
+$getData = $_GET;
+
+if (!isset($getData['id']) && is_numeric($getData['id']))
+{
+	echo("Il faut un identifiant d'arceau pour le modifier.");
+    return;
+}	
+
+$retrieveArceau = $db -> prepare('SELECT * FROM arceau WHERE id_arceau=:id');
+$retrieveArceau ->execute([
+    'id' => $getData['id'],
+]);
+
+$arceau = $retrieveArceau->fetch(PDO::FETCH_ASSOC);
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<!-------------- HEAD -------------->
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style_admin_page.css"  type="text/css"/>
+    <title>Modification Arceau</title>
+</head>
+
+
+<!-------------- FORMULAIRE DE MODIFCATION DE LA BASE DE DONNE --------------> 
+
+<form action="update_arceau_validation.php" method="post">
+    <?php if(isset($errorMessage)) : ?>
+    
+        <div class="alert alert-danger" role="alert">
+            <?php echo $errorMessage; ?>
+        </div>
+
+    <?php endif; ?>
+
+    <div class="coords">
+        <label for="latitude" class="form-label">Latitude</label>
+        <input type="number" class="form-control" id="latitude_add" name="latitude" step="0.0000001" placeholder="+/- 00.0000000", value= <?php echo($arceau['lattitude']); ?>>
+
+        <label for="longitude" class="form-label">Longitude</label>
+        <input type="number" class="form-control" id="longitude_add" name="longitude" step="0.0000001" placeholder="+/- 00.0000000", value= <?php echo($arceau['longitude']); ?>>
+    </div>
+
+    <div class="etat">
+        <label for="etat" class="form-label">État</label>
+        <input type="number" class="form-control" id="etat_add" name="etat"  placeholder="0 or 1", value= <?php echo($arceau['etat']); ?>>
+    </div>
+
+    <div class="utilisateur">
+        <label for="utilisateur" class="form-label">Utilisateur</label>
+        <input type="number" class="form-control" id="utilisateur_add" name="utilisateur"  placeholder="1", value= <?php echo($arceau['id_user']); ?>>
+    </div>
+
+    <div class="groupe">
+        <label for="groupe" class="form-label">Groupe</label>
+        <input type="number" class="form-control" id="groupe_add" name="groupe"  placeholder="1", value= <?php echo($arceau['groupe']); ?>>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Modifier</button>
+</form>
+
+<a href="//localhost/projet/admin_page.php"> Retour à la page d'administration </a>
+
