3467593f354c11ce6da2aaf6a55d6eb9b6f490a9
Yoann <thebaultyoann@gmail.com>
2023-01-02 19:47:49 +0100

    Ajout pour correction de bug

--- update_arceau_validation.php
+++ update_arceau_validation.php
@@ -1,25 +1,28 @@
 <?php
 session_start();
 include_once('./config_mysql.php');
+
+$getData = $_GET;
+
+if(!isset($getData['id']) || !isset($_POST['lattitude']) || !isset($_POST['longitude']) || !isset($_POST['etat']) || !isset($_POST['groupe']) || !isset($_POST['id_user'])){
+	echo 'Il manque des informations pour permettre l\'édition du formulaire.'.'<br>';
+    return;
+}
 ?>
-
-
 
 <!------ VALIDATION DU FORMULAIRE D'AJOUT ------->
 
 <?php
-if (isset($_POST['latitude']) || isset($_POST['longitude']) || isset($_POST['etat']) || isset($_POST['utilisateur']) || isset($_POST['groupe'])){
-
-/*ajouter une ligne pour vérifier que l'arceau n'est pas déjà dans la database et donc ne pas faire un doublon*/
-
-$sqlQuery2 = 'INSERT INTO arceau(lattitude,longitude,etat,id_user,groupe) VALUES(:lattitude,:longitude,:etat,:id_user,:groupe)';
-$insertArceau = $db -> prepare($sqlQuery2);
-$insertArceau -> execute([
-        'lattitude' => $_POST['latitude'],
+//ajouter une ligne pour vérifier que l'arceau n'est pas déjà dans la database et donc ne pas faire un doublon*/
+$sqlQuery2 = 'UPDATE arceau SET lattitude=:lattitude, longitude=:longitude, etat=:etat, groupe=:groupe, id_user=:id_user WHERE id_arceau=:id';
+$modifyArceau = $db -> prepare($sqlQuery2);
+$modifyArceau -> execute([
+        'lattitude' => $_POST['lattitude'],
         'longitude' => $_POST['longitude'],
         'etat' => $_POST['etat'],
-        'id_user' => $_POST['utilisateur'],
-        'groupe' => $_POST['groupe']
+        'groupe' => $_POST['groupe'],
+        'id_user' => $_POST['id_user'],
+        'id' => $getData['id']
 ]);
 
 ?>
@@ -41,34 +44,16 @@ $insertArceau -> execute([
     <div class="container">
         <h1>Modification de l'arceau réussie !</h1>
 
-     <?php echo ('Latitude :'.$_POST['latitude'])."<br>"; ?>
+     <?php echo ('Latitude :'.$_POST['lattitude'])."<br>"; ?>
      <?php echo ('Longitude :'.$_POST['longitude'])."<br>"; ?>
      <?php echo ('État :'.$_POST['etat'])."<br>"; ?>
-     <?php echo ('Utilisateur :'.$_POST['utilisateur'])."<br>"; ?>
+     <?php echo ('Utilisateur :'.$_POST['id_user'])."<br>"; ?>
      <?php echo ('Groupe :'.$_POST['groupe'])."<br>"; ?>
-
 
 
     </div>
 
     
     <a href="//localhost/projet/admin_page.php"> Retour à la page d'administration </a>
-    <br>
-    <a href="//localhost/projet/admin_page.php"> Retour à la page d'ajout d'un arceau </a>
 
 </body>
-
-<?php
-}
-else{
-    echo('il faut des données fournies qui respectent le format demandé');
-?>
-
-
-    <div class=boutton>
-        <a href="//localhost/projet/ajout_arceau.php"> Retour à la page d'administration </a>
-    </div>
-
-<?php
-}
-?>
